584c791cad8ed60f58c2c42e38f73a56aaae8583
Rodrigo Takeshi <rodrigo.takeshi@thoughtworks.com>
2023-10-30 16:50:06 -0300

    fix: solving key commit remove

---
+++ infra/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.85.0"
+    }
+  }
+}
+
+
+provider "google" {
+  #Configuration options
+  project = "terraform-gcp-402419"
+  region = "us-central1"
+  zone = "us-central1-a"
+  credentials = "keys.json"
+
+}
