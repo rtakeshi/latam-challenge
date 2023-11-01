a88b676508c9cf1708144cf59a943954e8ebfeb3
Rodrigo Takeshi <rodrigo.takeshi@thoughtworks.com>
2023-11-01 18:25:54 -0300

    feat: developing q1_memory, first commit to tdd test

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,11 @@
 from typing import List, Tuple
 from datetime import datetime
+from pyspark.sql import SparkSession
+
+
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    return None
+    
+    spark = SparkSession.builder.appName("FarmersProtestTweets").getOrCreate()
+    df = spark.read.csv(file_path, sep = "~")
+    return df.show(5)
